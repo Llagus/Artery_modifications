957da26cc623d6932bfcb293c09aa4ff593040d4
Llagus <llagust123@gmail.com>
2020-11-04 16:36:35 +0100

    tictoc6 implemented

---
+++ omnetpp-5.6.2/samples/tictoc_ll/txc6.cc
@@ -0,0 +1,78 @@
+#include <string.h>
+#include <omnetpp.h>
+#include <iostream>
+
+using namespace omnetpp; 
+
+class Txc6 : public cSimpleModule{
+    private: 
+        int count; 
+        cMessage *event; 
+        cMessage *tictocMsg; 
+    
+    public: 
+        Txc6(); 
+        virtual ~Txc6(); 
+
+
+    protected: 
+        virtual void initialize() override; 
+        virtual void handleMessage(cMessage * msg) override; 
+};
+
+Define_Module(Txc6); 
+
+Txc6::Txc6(){
+    // Set the pointer to nullptr, so that the destructor won't crash
+    // even if initialize() doesn't get called because of a runtime
+    // error or user cancellation during the startup process.
+    event=tictocMsg=nullptr; 
+}
+
+
+Txc6::~Txc6(){
+    // Dispose of dynamically allocated the objects
+    cancelAndDelete(event);
+    delete tictocMsg;  
+}
+
+void Txc6::initialize(){
+    //Create and event object we'll use for timing -- just an ordinary message. 
+    event = new cMessage("event");
+
+    //No tictocMsg yet
+    tictocMsg=nullptr; 
+
+    if (par("sendMsgOnInit").boolValue() == true){
+        // We don't start right away, but instead send an message to ourselves
+        // (a "self-message") -- we'll do the first sending when it arrives
+        // back to us, at t=5.0s simulated time.
+        EV << "Scheduling first send at t=5.0s\n";
+        tictocMsg = new cMessage("tictocMsg");
+        scheduleAt(5.0,event);  
+    } 
+}
+
+void Txc6::handleMessage(cMessage *msg){
+    // There are several ways of distinguishing messages, for example by message
+    // kind (an int attribute of cMessage) or by class using dynamic_cast
+    // (provided you subclass from cMessage). In this code we just check if we
+    // recognize the pointer, which (if feasible) is the easiest and fastest
+    // method.
+    if (msg == event){
+        // The self-message arrived, so we can send out tictocMsg and nullptr out
+        // its pointer so that it doesn't confuse us later.
+        EV << "Wait period is over, sending back message\n";
+        send(tictocMsg,"out"); 
+        tictocMsg=nullptr; 
+    }
+    else{
+        // If the message we received is not our self-message, then it must
+        // be the tic-toc message arriving from our partner. We remember its
+        // pointer in the tictocMsg variable, then schedule our self-message
+        // to come back to us in 1s simulated time.
+        EV << "Message arrived, starting to wait 1 sec...\n"; 
+        tictocMsg = msg; 
+        scheduleAt(simTime()+1.0,event); 
+    }
+}
